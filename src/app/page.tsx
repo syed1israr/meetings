9ae22a38261216b18aac65d6dfd4e5bfb6ace8b1
Israr <syedfaraz173@gmail.com>
2025-07-16 18:58:56 +0530

    Database

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Button
+      variant="default" >Hi</Button>
+  )
+}
+
+export default page
